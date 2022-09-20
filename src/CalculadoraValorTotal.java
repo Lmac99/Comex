ef1a4ceef11eadde48231e39b7984bd1f766dfe1
Lucas <lma2@icomp.ufam.edu.br>
2022-09-20 07:06:25 -0400

    Calculadora de valor total

---
+++ src/CalculadoraValorTotal.java
@@ -0,0 +1,11 @@
+public class CalculadoraValorTotal {
+    public double calcula(Produto prod){
+        double total = prod.getPreco() * prod.getQuantidade();
+        return total;
+    }
+
+    public double calcula(ItemPedido iPedido){
+        double total = iPedido.getPreco() * iPedido.getQuantidade();
+        return total;
+    }
+}
---
+++ src/MainCalculadoraValorTotal.java
@@ -0,0 +1,29 @@
+public class MainCalculadoraValorTotal {
+    public static void main(String[] args) {
+        CalculadoraValorTotal calc = new CalculadoraValorTotal();
+
+        // CLIENTES
+        Cliente cli1 = new Cliente("Ana", "001", "11 9 99999999", "Rua A", "12", "Apt 2", "Bairro A", "São Paulo",
+                "São Paulo");
+        Cliente cli2 = new Cliente("Eli", "002", "48 9 88888888", "Rua Azul", "N/S", "N/S", "Bairro Cinza",
+                "Florianópolis", "Santa Catarina");
+
+        // PEDIDOS FEITOS PELOS CLIENTES
+        Pedido p1 = new Pedido(cli1);
+        Pedido p2 = new Pedido(cli2);
+
+        // PRODUTOS COMPRADOS
+        Produto prod1 = new Produto("Notebook Samsung", "descricao", 3523.00, 1, "Informática");
+        Produto prod2 = new ProdutoIsento("Clean Architecture", "descricao", 102.90, 2, "Livros");
+
+        // COMPOSIÇÃO DO ITEM PEDIDO
+        ItemPedido iPedido1 = new ItemPedido(2899.99, 2, prod1, p1, "PROMOCAO");
+        ItemPedido iPedido2 = new ItemPedido(100, 11, prod2, p2, "QUANTIDADE");
+
+        System.out.println("Total produto "+ prod1.getId()+ ": " +calc.calcula(prod1));
+        System.out.println("Total produto "+ prod2.getId()+ ": " +calc.calcula(prod2));
+
+        System.out.println("Total Item pedido "+ iPedido1.getId()+ ": " +calc.calcula(iPedido1));
+        System.out.println("Total Item pedido "+ iPedido2.getId()+ ": " +calc.calcula(iPedido2));
+    }
+}
