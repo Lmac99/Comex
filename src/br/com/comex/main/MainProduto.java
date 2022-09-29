3375071fe9d15245cbad4ce5ab3ff168ef3cf0d9
Lucas <lma2@icomp.ufam.edu.br>
2022-09-29 11:37:29 -0400

    removendo produto não isento

--- src/br/com/comex/main/MainProduto.java
+++ src/br/com/comex/main/MainProduto.java
@@ -6,7 +6,6 @@ public class MainProduto {
         Categoria cat1 = new Categoria(1,"INFORMÁTICA", "ativa");
         Categoria cat2 = new Categoria(2,"LIVROS", "inativa");
         Produto prod1 = new Produto("Notebook Samsung", "descricao", 3523.00, 1, cat1);
-        //Produto prod2 = new Produto(2, "Clean Architecture", "descricao", 102.90, 2, "Livros"); <- exercício de criar o produto
         Produto prod2 = new ProdutoIsento("Clean Architecture", "descricao", 102.90, 2, cat2);
         Produto prod3 = new Produto("Monitor Dell 27", "descricao", 1889.00, 3, cat1);
 
